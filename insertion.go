63ffeb3ffa0e358aa8e0acfdff3e02f1f1de646f
Mahmoud Fredj <newraisingray@gmail.com>
2024-02-07 10:52:13 +0300

    wip insertion

---
+++ insertion.go
@@ -0,0 +1,16 @@
+package main
+
+Insertion(arr []int){
+	flagMove:=false
+	for i:=0;i<len(arr);i++{
+		if i!=0 && arr[i]>arr[i-1]	{
+			for k:=i-1;k>=0;k--{
+				if arr[i]< arr[k] {
+					flagMove=true;
+				} else if !flagMove{
+					arr[i],arr[k]=arr[k],arr[i]	
+				}
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,7 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{3, 4, 5, 6, 1, 2, 3, 6, 9, 3, 4, 8, 1, 2}
-	BubbleSort(arr)
-	fmt.Println(arr)
+	arr := []int{-1, 1, 0, -3, 3}
+	var res []int
+	res = productExceptSelf(arr)
+	fmt.Println(res)
+}
+func productExceptSelf(nums []int) []int {
+	var res []int
+	total := 1
+	for _, v := range nums {
+		if v != 0 {
+
+			total *= v
+		}
+	}
+	for _, v := range nums {
+		if v != 0 {
+			res = append(res, total/v)
+		} else {
+			res = append(res, total)
+		}
+
+	}
+	return res
 }
